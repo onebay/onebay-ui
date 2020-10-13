da9b45cb942450d7b5a98ab2a1a39d213fa1e625
pengsl <18718574603@163.com>
2020-10-14 00:13:06 +0800

    feat: add tag

---
+++ packages/swift-ui/src/components/tag/index.tsx
@@ -0,0 +1,94 @@
+import { computed, defineComponent, PropType } from 'vue'
+import classNames from 'classnames'
+
+export enum SIZE_CLASS {
+  normal = 'normal',
+  small = 'small'
+}
+
+export enum TYPE_CLASS {
+  primary = 'primary'
+}
+
+const Tag = defineComponent({
+  name: 'Tag',
+  props: {
+    size: {
+      type: String as PropType<SIZE_CLASS>,
+      default: 'normal',
+      validator: (val: string) => Object.keys(SIZE_CLASS).includes(val)
+    },
+    type: {
+      type: String as PropType<TYPE_CLASS>,
+      default: '',
+      validator: (val: string) => ['', 'primary'].includes(val)
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    customStyle: {
+      type: [Object, String],
+      default: ''
+    },
+    className: {
+      type: [Object, String],
+      default: ''
+    }
+  },
+  emits: ['click'],
+  setup(props) {
+    const rootCls = computed(() => {
+      const { size, type, circle, disabled, active } = props
+      const rootClassName = ['at-tag']
+      const classObject = {
+        [`at-tag--${SIZE_CLASS[size]}`]: size,
+        [`at-tag--${TYPE_CLASS[type]}`]: type,
+        'at-tag--disabled': disabled,
+        'at-tag--active': active,
+        'at-tag--circle': circle
+      }
+      return classNames(rootClassName, classObject, props.className)
+    })
+    return {
+      rootCls
+    }
+  },
+  methods: {
+    handleClick(event: Event): void {
+      if (!this.disabled) {
+        this.$emit(
+          'click',
+          {
+            name: this.name,
+            active: this.active
+          },
+          event
+        )
+      }
+    }
+  },
+  render() {
+    const { handleClick, rootCls, $slots } = this
+    const { customStyle } = this.$props
+    return (
+      <div class={rootCls} style={customStyle} onClick={handleClick}>
+        {$slots.default && $slots.default()}
+      </div>
+    )
+  }
+})
+
+export default Tag
