b5dfb57d4e80b7518fd295b06b29310f34a6c14a
pengsl <18718574603@163.com>
2020-12-11 01:06:33 +0800

    feat: add ssr-demo

---
+++ packages/ssr-demo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Button>onebay ui</Button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { Button } from "onebay-ui";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Button,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
